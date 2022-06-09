9839ed9de0990d9489ebadba6c68e4a199f38c16
Hugo Radepont <hugoradepont@gmail.com>
2022-06-09 09:29:01 +0200

    add country to database + insert + singleNameTableTrait

---
+++ database/migrations/2022_06_06_100131_create_country_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('country', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('english_name');
+            $table->string('official_name')->nullable();
+            $table->string('code');
+            $table->string('flag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('country');
+    }
+};
